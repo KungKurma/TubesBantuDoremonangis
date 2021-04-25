6e82f390afe9b86ebd6f8f73e9065a14c562783f
KungKurma <cocosaboko@protonmail.ch>
2021-04-25 21:39:56 +0700

    Create F05.py

---
+++ F05.py
@@ -0,0 +1,48 @@
+# Program Menambah Item
+
+# Menerima input untuk menambah gadget/consumable setelah dikonfirmasi valid
+# I.S. Menerima database gadget dan item, serta input informasi item baru (input item divalidasi dulu)
+# F.S. Item baru dimasukkan ke database gadget dan item bila input valid
+
+
+# KAMUS 
+# databaseG : list of list
+# databaseC : list of list
+
+# ID : string
+# Nama : string
+# Deskripsi : string
+# Jumlah : string
+# Rarity : string
+# tahun : integer
+
+
+
+# FUNGSI DAN PROSEDUR
+
+def masukanInteger(masuk):
+# Prosedur input jumlah beserta validasi
+
+# KAMUS LOKAL
+
+# ALGORITMA
+    pass
+
+
+# PROGRAM UTAMA
+def tambahItem(databaseG, databaseC):
+    # Meminta input user
+    pass
+
+"""
+Nanti ini hapus
+
+dummyList = [ ["G1","Parametric Transformer","Sering dilupakan",10,'A',2020],
+              ["G2","Iphone 20","Layarnya 40 inchi",2,'B',2054],
+              ["G3","Swrirly Chair","You spin me right round, baby, right round like a record baby, right round, rpund, round.",3,'C',1776],
+              ["G4","Stand Arrow","*insert any jojo meme*",3,'S',1987],
+              ["G5","Handphone Nokia","Benda terkuat di dunia",100,'B',2005],
+              ["G6","Go Go Gadget: Oil Slick","Prone to slip ups and combustion",2,'C',1989],
+              ["G6","SUPERSEMAR","Lah, kok di sini?",1,'S',1996] ]
+
+"""
