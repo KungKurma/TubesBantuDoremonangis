50009b65de6e68767948520ab72d4aed5a831482
darielgaizta <80236164+darielgaizta@users.noreply.github.com>
2021-04-28 21:12:53 +0700

    Update __init__.py

--- function/__init__.py
+++ function/__init__.py
@@ -1,3 +1,5 @@
+# __init__.py -> biar di main cukup "import function" (import semua modul)
+# Format: from .<nama_modul> import ...
+
 from .F0103 import *
 from .F1415 import *
-# tambahin lagi ges...
