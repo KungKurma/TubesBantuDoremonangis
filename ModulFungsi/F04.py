8e8f6e3ac63c521af101022f7aa11a5732689912
KungKurma <cocosaboko@protonmail.ch>
2021-05-01 10:49:41 +0700

    Update F04.py

--- ModulFungsi/F04.py
+++ ModulFungsi/F04.py
@@ -7,16 +7,16 @@
 
 # KAMUS 
 # database : list of list
-# tahun : integer
+# tahun    : integer
 # kategori : string
-# found : boolean
-# panjang : integer
+# found    : boolean 
+# panjang  : integer
 
 
 # FUNGSI DAN PROSEDUR
 
 def cetakSearch(Id, database):
-# Prosedur mencetak info gadget Id
+# Prosedur mencetak info gadget berdasarkan Id
 
 # KAMUS LOKAL
 
@@ -31,14 +31,13 @@ def cetakSearch(Id, database):
 
 # PROGRAM UTAMA
 def cariGadget(database):
-    database = database[1:]
+    database = database[1:]     # Menghilangkan bagian awal list yang berisi [id, nama, ...]
     
     # Meminta input user
-    tahun = int(input("\nMasukkan tahun: "))
+    tahun    = int(input("\nMasukkan tahun: "))
     kategori = input("Masukkan kategori: ")
-
-    found = False
-    panjang = len(database)
+    found    = False
+    panjang  = len(database)
 
     print("\nHasil Pencarian :\n")
 
@@ -76,16 +75,3 @@ def cariGadget(database):
     # Hasil cetak bila tidak ditemukan gadget sesuai dengan kategori pencarian
     if not(found):
         print("Tidak ada gadget yang ditemukan")
-
-"""
-Nanti ini hapus
-
-dummyList = [ ["G1","Parametric Transformer","Sering dilupakan",10,'A',2020],
-              ["G2","Iphone 20","Layarnya 40 inchi",2,'B',2054],
-              ["G3","Swrirly Chair","You spin me right round, baby, right round like a record baby, right round, rpund, round.",3,'C',1776],
-              ["G4","Stand Arrow","*insert any jojo meme*",3,'S',1987],
-              ["G5","Handphone Nokia","Benda terkuat di dunia",100,'B',2005],
-              ["G6","Go Go Gadget: Oil Slick","Prone to slip ups and combustion",2,'C',1989],
-              ["G6","SUPERSEMAR","Lah, kok di sini?",1,'S',1996] ]
-
-"""
