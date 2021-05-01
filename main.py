856daa2979215f41a98b332d8e2d94d49d3b3bb4
darielgaizta <80236164+darielgaizta@users.noreply.github.com>
2021-05-01 21:32:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # KAMUS
 # files = list nama file (csv)
 # db = matriks dari tiap isi file (csv)
+# command, action, path = string
+# save_last_changes = character
 
 from ModulFungsi import *
 
@@ -28,37 +30,36 @@ db_user = convert(db[5], Int=True)
 
 while 1:
 	command = input('>>> ')
-	if command == 'login':									# F02
+	if command == 'login':								# F02
 		user = login(db_user)
 		while user != False:
 			action = input('>>> ')
-			if action == 'register' and user[5] == 'Admin':				# F01 (Admin only)
+			if action == 'register' and user[5] == 'Admin':			# F01 (Admin only)
 				path = get_path('user.csv')
 				register(path,db_user)
-			elif action == 'carirarity':						# F03
+			elif action == 'carirarity':					# F03
 				carirarity(db_gadget)
-			elif action == 'caritahun':						# F04
+			elif action == 'caritahun':					# F04
 				cariTahun(db_gadget)
-			elif action == 'tambahitem' and user[5] == "Admin":			# F05 (Admin only)
+			elif action == 'tambahitem' and user[5] == "Admin":		# F05 (Admin only)
 				db_gadget, db_consumable = tambahItem(db_gadget, db_consumable)
-			elif action == 'hapusitem' and user[5] == "Admin":			# F06 (Admin only)
+			elif action == 'hapusitem' and user[5] == "Admin":		# F06 (Admin only)
 				db_gadget, db_consumable = hapusItem(db_gadget, db_consumable)
-			elif action == 'ubahjumlah' and user[5] == "Admin":			# F07 (Admin only)
+			elif action == 'ubahjumlah' and user[5] == "Admin":		# F07 (Admin only)
 				ubah_jumlah(db_gadget, db_consumable)
-			elif action == 'pinjam' and user[5] == "User":				# F08 (User only)
+			elif action == 'pinjam' and user[5] == "User":			# F08 (User only)
 				meminjam_gadget(user[0],db_gadget_borrow_history, db_gadget)
-			elif action == 'kembalikan' and user[5] == "User":			# F09 (User only)
+			elif action == 'kembalikan' and user[5] == "User":		# F09 (User only)
 				mengembalikan_gadget(user[0], db_gadget_borrow_history, db_gadget, db_gadget_return_history)
-			elif action == 'minta' and user[5] == "User":				# F10 (User only)
+			elif action == 'minta' and user[5] == "User":			# F10 (User only)
 				hasil_minta = minta(db_consumable, db_consumable_history, user)
 				db_consumable_history = hasil_minta[1]
 			elif action == 'riwayatpinjam' and user[5] == "Admin":		# F11 (Admin only)
 				riwayatpinjam(db_gadget_borrow_history, db_user, db_gadget, user)
 			elif action == 'riwayatkembali' and user[5] == "Admin":		# F12 (Admin only)
 				riwayatkembali(db_gadget_return_history, db_user, db_gadget, user)
-			elif action == 'riwayatambil' and user [5] == "Admin":
-				riwayatambil(db_consumable_history,db_user, db_consumable, user)    # F13 (Admin only)
-
+			elif action == 'riwayatambil' and user [5] == "Admin":		# F13 (Admin only)
+				riwayatambil(db_consumable_history,db_user, db_consumable, user)
 			elif action == 'save':						# F15
 				db[0] = convert(db_consumable,Str=True)
 				db[1] = convert(db_consumable_history,Str=True)
