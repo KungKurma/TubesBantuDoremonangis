97fd22a81d570b57d0c98d90680aaef09e7d45d4
KungKurma <cocosaboko@protonmail.ch>
2021-05-01 10:50:39 +0700

    Update __init__.py

--- ModulFungsi/__init__.py
+++ ModulFungsi/__init__.py
@@ -1,11 +1,11 @@
 # __init__ agar di main.py hanya import package ModulFungsi
 
 from .F0103 import *
-from .F1415 import *
-from .F07 import *
-from .F08 import *
-from .F09 import *
 from .F04 import *
 from .F05 import *
 from .F06 import *
+from .F07 import *
+from .F08 import *
+from .F09 import *
+from .F1415 import *
 # from .F(...) import (...)
