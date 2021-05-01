c117d76f6f83fae835d10d80a6b7c57b4076ae56
KungKurma <cocosaboko@protonmail.ch>
2021-05-01 10:49:44 +0700

    Update F05.py

--- ModulFungsi/F05.py
+++ ModulFungsi/F05.py
@@ -5,19 +5,12 @@
 
 
 # KAMUS 
-# databaseG : list of list
+# databaseG : list of list 
 # databaseC : list of list
+"""Nanti ganti data typenya"""
 
-"""
-Kommen yang benar
-"""
-# ID : string
 # isGadget : boolean
-# Nama : string
-# Deskripsi : string
-# Jumlah : string
-# Rarity : string
-# Tahun : integer
+# ID, Nama, Deskripsi, Jumlah, Rarity, Tahun     : string
 # Item : list
 
 
@@ -28,20 +21,18 @@ def searchDatabase(Id, database):
 
 # KAMUS LOKAL
 # found : boolean
-    """
-    Kommen yang benar
-    """
+
 # ALGORITMA
     found = False
     for i in database:
-        if i[0] == Id:
+        if i[0] == Id: 
             found = True
             break
     return found
 
 
 def rarityValid(x):
-# Fungsi yang memeriksa apakah input integer
+# Fungsi yang memeriksa apakah x input rarity yang valid
 
 # KAMUS LOKAL
 
@@ -50,33 +41,32 @@ def rarityValid(x):
 
 
 def sortInsert(Database, Entry):
-# Fungsi yang memeriksa apakah input integer
+# Fungsi memasukan entry baru ke database sesuai urutan ID
 
-    """
-    Kommen yang benar
-    """
 # KAMUS LOKAL
 # panjang : integer
-# idNo : integer
+# idNo    : integer
 # entryNo : integer
-# temp : list
+# temp    : list of list
 
 # ALGORITMA
-    temp = [Database[0]]
-    entNo = int(Entry[0][1:])
+    temp    = [Database[0]]    # Memasukkan bagian awal ([id, nama, deskripsi, ...])
+    entNo   = int(Entry[0][1:])   # Nomor id Entry
     panjang = len(Database) - 1
 
-    for i in range(panjang):
-        idNo = int(Database[i+1][0][1:])
+    for i in range(1,(panjang + 1)):
+        idNo = int(Database[i][0][1:])    # Nomor id dari rekaman i
         
-        if (entNo < idNo):
+        if (entNo < idNo):  # Jika entry mempunyai urutan di tengah
             temp.append(Entry)
-            for j in Database[(i+1):]:
+            for j in Database[i:]:  # Memasukkan sisa dari database
                 temp.append(j)
             break
+
         else:
-            temp.append(Database[i+1])
-            if  i == (panjang):
+            temp.append(Database[i]) 
+
+            if  i == panjang: # Jika entry mempunyai urutan terakhir
                 temp.append(Entry)
     
     return temp
@@ -84,68 +74,44 @@ def sortInsert(Database, Entry):
 
 # PROGRAM UTAMA
 def tambahItem(databaseG, databaseC):
-    # Input ID
     ID = input("Masukan ID: ")
     
     # Pengecekan ID valid
-    if ((ID[0] != "G") and (ID[0] != "C")) or not( ID[1:].isdigit() ) :
+    if ((ID[0] != "G") and (ID[0] != "C")) or not( ID[1:].isdigit() ) :     # Masukkan sudah ada di database
         print("\nGagal menambahkan item karena ID tidak valid.")
-        return (databaseG, databaseC)
 
-    if (searchDatabase(ID, databaseC) or searchDatabase(ID, databaseG)):
+    elif (searchDatabase(ID, databaseC) or searchDatabase(ID, databaseG)):  # Masukkan sudah ada di database
         print("\nGagal menambahkan item karena ID sudah ada.")
-        return (databaseG, databaseC)
     
-    isGadget = (ID[0] == "G")
-        
-    Nama = input("Masukan Nama: ")
+    else:            
+        Nama = input("Masukan Nama: ")
+        Deskripsi = input("Masukan Deskripsi: ")
+        Jumlah = input("Masukan Jumlah: ")
 
-    Deskripsi = input("Masukan Deskripsi: ")
+        if not(Jumlah.isdigit()):   # Masukkan bukan integer
+            print("\nInput jumlah tidak valid!")
 
-    Jumlah = input("Masukan Jumlah: ")
-    if not(Jumlah.isdigit()):
-        print("\nInput jumlah tidak valid!")
-        return (databaseG, databaseC)
-            
-    Rarity = input("Masukan Rarity: ")
-    if not(rarityValid(Rarity)):
-        print("\nInput rarity tidak valid!")
-        return (databaseG, databaseC)
+        else:
+            Rarity = input("Masukan Rarity: ")
 
-    if isGadget:
-        Tahun = input("Masukan tahun ditemukan: ")
-        if not(Tahun.isdigit()):
-            print("\nInput tahun tidak valid!")
-            return (databaseG, databaseC)
-        
-        Item = [ID, Nama, Deskripsi, Jumlah, Rarity, Tahun]
-        databaseG = sortInsert(databaseG, Item)
-        print("\nItem telah berhasil ditambahkan ke database.")
-        return (databaseG, databaseC)
-    
-    else:
-        Item = [ID, Nama, Deskripsi, Jumlah, Rarity]
-        databaseC = sortInsert(databaseC, Item)
-        print("\nItem telah berhasil ditambahkan ke database.")
-        return (databaseG, databaseC)
-    
+            if not(rarityValid(Rarity)):    # Masukkan bukan rarity yang valid
+                print("\nInput rarity tidak valid! (Rarity yang ada adalah S, A, B, C)")
 
-        
+            else:
+                if (ID[0] == "G"):    # Jika item baru berupa gadget
+                    Tahun = input("Masukan tahun ditemukan: ")
 
+                    if not(Tahun.isdigit()):    # Masukkan bukan integer
+                        print("\nInput tahun tidak valid!")
 
-
-dumG = [ ["G1","Parametric Transformer","Sering dilupakan",10,'A',2020],
-         ["G2","Iphone 20","Layarnya 40 inchi",2,'B',2054],
-         ["G3","Swrirly Chair","You spin me right round, baby, right round like a record baby, right round, rpund, round.",3,'C',1776],
-         ["G4","Stand Arrow","*insert any jojo meme*",3,'S',1987],
-         ["G5","Handphone Nokia","Benda terkuat di dunia",100,'B',2005],
-         ["G6","Go Go Gadget: Oil Slick","Prone to slip ups and combustion",2,'C',1989],
-         ["G8","SUPERSEMAR","Lah, kok di sini?",1,'S',1996]]
-
-dumC = [ ["C1","Parametric Transformer","Sering dilupakan",10,'A'],
-         ["C2","Iphone 20","Layarnya 40 inchi",2,'B'],
-         ["C3","Swrirly Chair","You spin me right round, baby, right round like a record baby, right round, rpund, round.",3,'C'],
-         ["C4","Stand Arrow","*insert any jojo meme*",3,'S'],
-         ["C5","Handphone Nokia","Benda terkuat di dunia",100,'B'],
-         ["C6","Go Go Gadget: Oil Slick","Prone to slip ups and combustion",2,'C'],
-         ["C8","SUPERSEMAR","Lah, kok di sini?",1,'S'] ]
+                    else:
+                        Item = [ID, Nama, Deskripsi, Jumlah, Rarity, Tahun]
+                        databaseG = sortInsert(databaseG, Item)
+                        print("\nItem telah berhasil ditambahkan ke database.")
+                
+                else:   # Jika item baru berupa consumable
+                    Item = [ID, Nama, Deskripsi, Jumlah, Rarity]
+                    databaseC = sortInsert(databaseC, Item)
+                    print("\nItem telah berhasil ditambahkan ke database.")
+                    
+    return (databaseG, databaseC)
